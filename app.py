3e86331baf44149d5d5aa1c8907169cbfa10bc33
cyrus <cywong@vtc.edu.hk>
2018-06-19 04:11:42 -0400

    Lab03

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from config import Config
+from forms import LoginForm
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
 
 
 @app.route('/')
@@ -18,6 +22,16 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
 
 if __name__ == '__main__':
