eb93872c327d762816c777fb2774ef82c9d23f9d
cyrus <cywong@vtc.edu.hk>
2018-07-03 03:03:11 -0400

    Lab13

--- config.py
+++ config.py
@@ -14,4 +14,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
+    LANGUAGES = ['en', 'es', 'zh']
     POSTS_PER_PAGE = 25
